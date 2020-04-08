554fb028988f95bb9dc5b5727b038a0c06caefc0
Allie LAIRLAB server <allie.delgiorno@gmail.com>
2020-04-08 07:49:55 -0400

    able to overfit to cat image with two masks (primary and secondary) per box

--- train_two_masks_one_image.py
+++ train_two_masks_one_image.py
@@ -1,27 +1,24 @@
 """
-The goal of single_image_overfit_one_proposal is to prove we can overfit to a single selected proposal.  Reasoning:
-- With the customized head, we need to specialize the loss with respect to each proposal
-- With the customized head, we may be using only a subset of the proposals, or a different set entirely (interfacing
-at an earlier level).  By fitting to a single proposal, we're demonstrating that we are able to interface at the
-proposal rather than image level.
-- Our next step will be to change this loss to the customized loss.
+The goal of train_two_masks_one_image is to overfit one image to two masks per box.  For the two cats - one chair
+image, this means each cat box predicts two cat masks, and the chair box predicts on chair.
+
+Step 1a: Modify multimask loss to send two different sets of weights / ground truth to the loss function
+Step 1b: Verify with multimask inference to see that the set of weights is learning the primary or secondary mask,
+depending on which one we switch between.
+
 """
 
-import cv2
-import numpy as np
 import os
 import torch
 import torch.distributed
+import torch.distributed
 
+import script_utils
+import vis_utils
 from detectron2.data import MetadataCatalog
-import vis_utils, script_utils
 from detectron2.utils.events import EventStorage
-from script_utils import get_custom_maskrcnn_cfg, get_datapoint_file, \
-    convert_datapoint_to_image_format, run_inference, visualize_instancewise_predictions, prep_image, equal_ids, \
-    activate_head_type
-from vis_utils import FigExporter
+from script_utils import run_inference, visualize_instancewise_predictions, prep_image
 from trainer_apd import Trainer_APD
-from detectron2.modeling.roi_heads.multi_mask_head_apd import CustomMaskRCNNConvUpsampleHeadAPD
 
 exporter_ = None
 
@@ -30,104 +27,105 @@ def dbprint(*args, **kwargs):
     print(*args, **kwargs)
 
 
-def get_formatted_boxes_matched_to_predictions(predictor, batch):
-    out_d = run_inference(predictor, batch)
-    assert len(batch) == 1
-    bi = 0  # index into batch
-    # 'extra_proposal_details' contains 'selected_proposal_idxs', 'boxes' (1000,4*80), and 'scores' (1000, 81)
-    selected_proposal_idxs = out_d['extra_proposal_details']['selected_proposal_idxs'][bi]
-    chosen_proposals = out_d['proposalss'][bi][selected_proposal_idxs]
-    predictions = out_d['outputs'][bi]['instances']
-    chosen_proposals.pred_classes = predictions.pred_classes
-    chosen_proposals.pred_boxes = chosen_proposals.proposal_boxes
-    return chosen_proposals, predictions
-
-
 def train_on_single_image(trainer: Trainer_APD, datapoint, max_itr=100, start_itr=0):
     assert max_itr >= start_itr
     with EventStorage(0) as trainer.storage:
-        for t in range(max_itr - start_itr):
+        for t in range(start_itr, max_itr):
+            if (t - start_itr) % 10 == 0:
+                print(t, '/', max_itr)
             trainer.run_step_with_given_data(datapoint)
 
 
-def main(image_id='486536', max_iters=(1,), exporter: vis_utils.FigExporter = None, head_type='custom'):
-    exporter = exporter or FigExporter()
+def main(image_id='486536', maxitr=500):
+    exporter = vis_utils.FigExporter()
 
     print('Beginning setup...')
-    cfg = get_custom_maskrcnn_cfg()
+    cfg = script_utils.get_custom_maskrcnn_cfg()
     trainer = Trainer_APD(cfg)
 
     print('Completing setup...')
     standard_head = trainer.model.roi_heads.mask_heads['standard']
     custom_head = trainer.model.roi_heads.mask_heads['custom']
-    if not torch.equal(standard_head.predictor.bias,
-                       custom_head.predictor.bias[::custom_head.num_instances_per_class, ...]):
-        raise Exception
-    if not torch.equal(standard_head.predictor.weight,
-                       custom_head.predictor.weight[::custom_head.num_instances_per_class, ...]):
-        raise Exception
 
-    datapoint = torch.load(get_datapoint_file(cfg, image_id))
+    datapoint = torch.load(script_utils.get_datapoint_file(cfg, image_id))
 
     if type(datapoint) is list:
         datapoint = datapoint[0]
     batch = [datapoint]
 
     # head_type = 'custom' if custom_head else 'standard'
-    activate_head_type(trainer, head_type)
+    head_type = 'custom'
+    script_utils.activate_head_type(trainer, head_type)
     cfg_tag = f"_{head_type}"
-    print('Pretraining')
-    custom_outputs_d = pretraining(batch, cfg, cfg_tag, exporter, image_id, trainer)
-    if 0:
-        head_type = 'standard'
-        cfg_tag = f"_{head_type}"
-        activate_head_type(trainer, head_type)
-        standard_outputs_d = pretraining(batch, cfg, cfg_tag, exporter, image_id, trainer)
-    cfg_tag = f"_{head_type}"
-    n_itrs = 0
 
     metadata = MetadataCatalog.get(cfg.DATASETS.TRAIN[0])
     names = metadata.get("thing_classes", None)
     labels = [names[i] for i in datapoint['instances'].gt_classes]
-    for maxitr in sorted(max_iters):
-        for dpt in batch:
-            # Train
-            print('Training on single image with {} steps'.format(maxitr))
-            train_on_single_image(trainer, batch, max_itr=maxitr, start_itr=n_itrs)
 
-            # Check inference
-            input_image = prep_image(dpt, cfg)
-            posttraining(cfg, cfg_tag, dpt, exporter, image_id, input_image, maxitr, trainer)
-            n_itrs += maxitr
+    for masks_key in ['pred_masks1', 'pred_masks2']:
+        pretraining(batch, cfg, cfg_tag + '_' + masks_key.replace('pred_masks', 'm'), exporter, image_id, trainer,
+                    masks_key=masks_key)
+
+    input_image = prep_image(batch[0], cfg)
+    step = 50
+    strt = 0
+    for strtitr in range(0, maxitr, step):
+        itr = strtitr + step
+        train_on_single_image(trainer, batch, max_itr=strtitr + itr, start_itr=strtitr)
+        # visualize
+        for masks_key in ['pred_masks1', 'pred_masks2']:
+            posttraining(cfg, cfg_tag + '_' + masks_key.replace('pred_masks', 'm'), batch[0], exporter, image_id,
+                         input_image, itr, trainer, masks_key=masks_key)
 
 
-def posttraining(cfg, cfg_tag, dpt, exporter, image_id, input_image, max_iters, trainer):
+def posttraining(cfg, cfg_tag, dpt, exporter, image_id, input_image, max_iters, trainer, masks_key='pred_masks',
+                 show_pipeline=False):
     outputs_d = run_inference(trainer, [dpt])
+    if masks_key is None:
+        pass
+    else:
+        assert masks_key.startswith('pred_masks')
+        for o in outputs_d['outputs']:
+            o['instances'].set('pred_masks', o['instances'].get(masks_key))
     outputs = outputs_d['outputs'][0]
+
     img, pred_instances, _ = script_utils.prep_for_visualization(cfg, input_image,
                                                                  pred_instances=outputs['instances'],
                                                                  proposals=None)
     posttrain_tag = f'_posttrain_{max_iters}'
-    visualize_instancewise_predictions(img, pred_instances, cfg, exporter, tag=f'{image_id}' + posttrain_tag +
-                                                                               cfg_tag)
+    visualize_instancewise_predictions(
+        img, pred_instances, cfg, exporter, tag=f'{image_id}' + posttrain_tag + cfg_tag)
     figure_name = image_id + cfg_tag + posttrain_tag + '_collated'
     figname = exporter.collate_previous('{:02d}'.format(exporter.fig_number) + 'c_' + figure_name,
                                         delete_individuals=True)
     dbprint('Exported', figname)
-    script_utils.run_single_image_results_visualization(cfg, exporter, outputs_d['extra_proposal_details'][0],
-                                                        image_id, img, trainer.model, outputs, outputs_d[
-                                                            'proposalss'][0],
-                                                        visualize_just_image=False)
-    figname = exporter.collate_previous('{:02d}'.format(exporter.fig_number) + 'c_' + os.path.splitext(os.path.basename(__file__))[0] + '_' + image_id + cfg_tag +
-                                        posttrain_tag + '_pipeline', delete_individuals=True)
-    dbprint('Exported', figname)
+    if show_pipeline:
+        script_utils.run_single_image_results_visualization(cfg, exporter, outputs_d['extra_proposal_details'][0],
+                                                            image_id, img, trainer.model, outputs, outputs_d[
+                                                                'proposalss'][0],
+                                                            visualize_just_image=False)
+        figname = exporter.collate_previous(
+            '{:02d}'.format(exporter.fig_number) + 'c_' + os.path.splitext(os.path.basename(__file__))[
+                0] + '_' + image_id + cfg_tag +
+            posttrain_tag + '_pipeline', delete_individuals=True)
+        dbprint('Exported', figname)
 
 
-def pretraining(batch, cfg, cfg_tag, exporter, image_id, trainer):
+def pretraining(batch, cfg, cfg_tag, exporter, image_id, trainer, masks_key=None):
     outputs_d = run_inference(trainer, batch)
+    if masks_key is None:
+        pass
+    else:
+        assert masks_key.startswith('pred_masks')
+        for o in outputs_d['outputs']:
+            o['instances'].set('pred_masks', o['instances'].get(masks_key))
+            o['instances'].pred_masks = o['instances'].get(masks_key)
+            assert torch.all(o['instances'].pred_masks == o['instances'].get(masks_key))
+
     for dpt, outputs in zip(batch, outputs_d['outputs']):
         vis_utils.visualize_instancewise_groundtruth(dpt, cfg, exporter, cfg_tag + '_gt')
-        exporter.collate_previous('{:02d}'.format(exporter.fig_number) + 'c_' + image_id + '_gt', delete_individuals=True)
+        exporter.collate_previous('{:02d}'.format(exporter.fig_number) + 'c_' + image_id + '_gt',
+                                  delete_individuals=True)
         input_image = prep_image(dpt, cfg)
         img, pred_instances, _ = script_utils.prep_for_visualization(cfg, input_image,
                                                                      pred_instances=outputs['instances'],
@@ -137,19 +135,19 @@ def pretraining(batch, cfg, cfg_tag, exporter, image_id, trainer):
                                                                                    cfg_tag)
         figure_name = os.path.splitext(os.path.basename(__file__))[0] + '_' + image_id + cfg_tag + \
                       posttrain_tag + '_collated'
-        figname = exporter.collate_previous('{:02d}'.format(exporter.fig_number) + 'c_' + figure_name, delete_individuals=True)
+        figname = exporter.collate_previous('{:02d}'.format(exporter.fig_number) + 'c_' + figure_name,
+                                            delete_individuals=True)
         dbprint('Exported', figname)
         script_utils.run_single_image_results_visualization(cfg, exporter, outputs_d['extra_proposal_details'][0],
                                                             image_id, img, trainer.model, outputs, outputs_d[
                                                                 'proposalss'][0],
                                                             visualize_just_image=False)
-        figname = exporter.collate_previous('{:02d}'.format(exporter.fig_number) + 'c_' + os.path.splitext(os.path.basename(__file__))[0] + '_' + image_id + cfg_tag +
-                                            posttrain_tag + '_pipeline', delete_individuals=True)
+        figname = exporter.collate_previous(
+            '{:02d}'.format(exporter.fig_number) + 'c_' + os.path.splitext(os.path.basename(__file__))[
+                0] + '_' + image_id + cfg_tag +
+            posttrain_tag + '_pipeline', delete_individuals=True)
     return outputs_d
 
 
 if __name__ == '__main__':
-    exporter_ = FigExporter()
-    image_ids_ = ['486536', '306284', '9']
-    for head_type in ['custom', 'standard']:
-        main(image_id=image_ids_[0], max_iters=[10, 100], exporter=exporter_, head_type=head_type)
+    main()
