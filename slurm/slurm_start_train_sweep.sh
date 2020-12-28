865811c126d7b316be2eed62104163d952d99151
allie-on-espresso <adelgior@cs.cmu.edu>
2020-12-28 16:35:29 -0500

    rbugfix: requesting GPUs now in slurm_start__train_sweep

--- slurm/slurm_start_train_sweep.sh
+++ slurm/slurm_start_train_sweep.sh
@@ -9,5 +9,5 @@ for file in ./configs/2020_12_26_d2s/*.yaml; do
     outst="/home/adelgior/data/slurm-logs/${datetime}-${basenm}"
     echo "redirected output to file starting with:"
     echo $outst
-    srun --job-name="$basenm" -o "$outst-job-%j.out" -p long bash ./train_bash_wrapper.sh --gpus 0,1,2,3 --config $file &
+    srun --gres=gpu:4 --job-name="$basenm" -o "$outst-job-%j.out" -p long bash ./train_bash_wrapper.sh --gpus 0,1,2,3 --config $file &
 done
