0996936499283212bbc750becf0455b3e23a5419
allie-on-espresso <adelgior@cs.cmu.edu>
2021-01-23 18:13:00 -0500

    added val_bash_wrapper (haven't used yet)

---
+++ val_bash_wrapper.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+nvidia-smi
+
+if [ "$1" == "--gpus" ]; then
+    shift;
+    gpus=$1
+    shift;
+fi
+source ~/.bashrc
+#export CUDA_VISIBLE_DEVICES=$gpus
+activate_virtualenv pytorch4
+echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
+cmd="python validate_trained_model.py $@"
+echo "$cmd"
+
+$cmd
+
+echo "Slurm job complete"
