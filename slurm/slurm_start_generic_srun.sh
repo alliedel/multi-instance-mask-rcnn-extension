3c120c5a113a0c4d7041cb4352f746eef0b3b115
allie-on-espresso <adelgior@cs.cmu.edu>
2021-03-27 23:00:44 -0400

    generic run for caching datasett stats

--- slurm/slurm_start_generic_srun.sh
+++ slurm/slurm_start_generic_srun.sh
@@ -3,4 +3,4 @@ datetime=`date +"%m-%d-%Y-%H-%M-%s"`
 outst="/home/adelgior/data/slurm-logs/${datetime}-${basenm}"
 echo "redirected output to file starting with:"
 echo $outst
-srun --gres=gpu:1 --job-name="generic" -o "$outst-job-%j.out" -p long bash ./slurm/generic_wrapper.sh --gpus 0 python scripts/assemble_model_comparison_galleries.py
+srun --gres=gpu:1 --job-name="generic" -o "$outst-job-%j.out" -p long bash ./slurm/generic_wrapper.sh --gpus 4 python scripts/cache_dataset_statistics.py
