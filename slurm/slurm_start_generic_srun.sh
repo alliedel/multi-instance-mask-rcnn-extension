a01d7377cb9af6c51edfe13d04db1e2bcd45e4c0
allie-on-espresso <adelgior@cs.cmu.edu>
2021-03-27 23:09:16 -0400

    fixed num GPUs, slurm log date format

--- slurm/slurm_start_generic_srun.sh
+++ slurm/slurm_start_generic_srun.sh
@@ -1,6 +1,6 @@
 i=$((i+1))
-datetime=`date +"%m-%d-%Y-%H-%M-%s"`
+datetime=`date +"%Y-%m-%d-%H-%M-%s"`
 outst="/home/adelgior/data/slurm-logs/${datetime}-${basenm}"
 echo "redirected output to file starting with:"
 echo $outst
-srun --gres=gpu:1 --job-name="generic" -o "$outst-job-%j.out" -p long bash ./slurm/generic_wrapper.sh --gpus 4 python scripts/cache_dataset_statistics.py
+srun --gres=gpu:4 --job-name="generic" -o "$outst-job-%j.out" -p long bash ./slurm/generic_wrapper.sh --gpus 4 python scripts/cache_dataset_statistics.py &
--- slurm/slurm_start_vis_sweep.sh
+++ slurm/slurm_start_vis_sweep.sh
@@ -10,7 +10,7 @@ while IFS= read -r line; do
     cfgpth="${line}/config.yaml"
     noparent="${cfgpth##*/}"
     basenm="${noparent%.yaml}"
-    datetime=`date +"%m-%d-%Y-%H-%M-%s"`
+    datetime=`date +"%Y-%m-%d-%H-%M-%s"`
     outst="/home/adelgior/data/slurm-logs/${datetime}-vis-${basenm}"
     cmd="srun --gres=gpu:1 --job-name=$basenm -o $outst-job-%j.out -p long -t 48:00:00 bash ./vis_bash_wrapper.sh --gpus 0 $line &"
     echo "$cmd"
