9a384afbed7e5a548db06495ba90a52031bb1a00
Allie LAIRLAB server <allie.delgiorno@gmail.com>
2021-03-28 22:45:28 -0400

    added script to run multiple val jobs on kalman (where there is no slurm)

---
+++ nonslurm_python_cmds.sh
@@ -0,0 +1,4 @@
+#python val.py --trained-logdir output/logs/train/train_2021-03-27-232834_VCS-021a9a3_MAX_ITR-500000_HEAD_TYPE-custom_MATCH-0 --rel-model-pth model_checkpoints --val-dataset coco_2017_val
+python val.py --trained-logdir output/logs/train/train_2021-03-27-232834_VCS-021a9a3_MAX_ITR-500000_HEAD_TYPE-custom_MATCH-0 --rel-model-pth model_checkpoints --val-dataset coco_2017_val
+python val.py --trained-logdir output/logs/train/train_2021-03-27-232834_VCS-021a9a3_MAX_ITR-500000_HEAD_TYPE-custom_MATCH-0 --rel-model-pth model_checkpoints --val-dataset coco_2017_train
+python val.py --trained-logdir output/logs/train/train_2021-03-27-232835_VCS-021a9a3_MAX_ITR-500000_HEAD_TYPE-custom_MATCH-1 --rel-model-pth model_checkpoints --val-dataset coco_2017_train
