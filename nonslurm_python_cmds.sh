a1e8580ef4c970447499e2bd282f54061254f1ca
Allie LAIRLAB server <allie.delgiorno@gmail.com>
2021-05-03 18:31:15 -0400

    updated nonslurm_python_cmds (for running validation directly)

--- nonslurm_python_cmds.sh
+++ nonslurm_python_cmds.sh
@@ -6,4 +6,5 @@
 #python val.py --trained-logdir output/logs/train/train_2021-03-29-232845_VCS-021a9a3_MAX_ITR-500000_HEAD_TYPE-None --rel-model-pth model_checkpoints/model_016000.pth.tar --val-dataset coco_2017_val
 #python val.py --trained-logdir output/logs/train/train_2021-03-29-232845_VCS-021a9a3_MAX_ITR-500000_HEAD_TYPE-None --rel-model-pth model_checkpoints --val-dataset coco_2017_val
 #python val.py --trained-logdir output/logs/train/train_2021-03-29-232845_VCS-021a9a3_MAX_ITR-500000_HEAD_TYPE-None --rel-model-pth model_checkpoints --val-dataset coco_2017_train
-python val.py --trained-logdir output/logs/train/train_2021-03-29-232845_VCS-021a9a3_MAX_ITR-500000_HEAD_TYPE-None --rel-model-pth model_checkpoints/model_000000.pth.tar --val-dataset coco_2017_val
+#python val.py --trained-logdir output/logs/train/train_2021-03-29-232845_VCS-021a9a3_MAX_ITR-500000_HEAD_TYPE-None --rel-model-pth model_checkpoints/model_000000.pth.tar --val-dataset coco_2017_val
+python val.py --trained-logdir output/logs/train/dummytest --rel-model-pth state_dict_trainerapd_170.pt --val-dataset coco_2017_val
