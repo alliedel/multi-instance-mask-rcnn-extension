6e1c128d17132aea21e892f83194b541817077ac
Allie LAIRLAB server <allie.delgiorno@gmail.com>
2021-06-18 16:38:47 -0400

    a bunch of analysis scripts

---
+++ multimaskextension/analysis/ipynboffload/utils20210520.py
@@ -0,0 +1,15 @@
+# [('DATALOADER', 'REPEAT_THRESHOLD'),
+#  ('MODEL', 'ROI_MASK_HEAD', 'MATCHING_LOSS'),
+#  ('MODEL', 'WEIGHTS'),
+#  ('SOLVER', 'BASE_LR'),
+#  ('SOLVER', 'IMS_PER_BATCH'),
+#  ('SOLVER', 'MAX_ITER')]
+
+# manual_cfg_keys = [
+#     ['SOLVER', 'IMS_PER_BATCH'],
+#     ['SOLVER', 'BASE_LR'],
+#     ['DATALOADER', 'REPEAT_THRESHOLD'],
+#     ['MODEL','ROI_HEADS', 'NAME'],
+#     ['MODEL','ROI_MASK_HEAD', 'INIT_ACTIVATED_MASK_HEAD'],
+#     ['MODEL', 'ROI_MASK_HEAD', 'MATCHING_LOSS']
+# ]
